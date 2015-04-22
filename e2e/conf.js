39a9da5280e63bc82063437d6babeae0cd612750
Mykhailo Miroshnikov <mykhailo.miroshnikov@gmail.com>
2015-04-22 15:43:37 +0300

    Remove animations in testing environment

--- e2e/conf.js
+++ e2e/conf.js
@@ -1,5 +1,19 @@
 exports.config = {
 	seleniumAddress: 'http://localhost:4444/wd/hub',
 	specs: './tests/**/*.spec.js',
-	framework: 'jasmine2'
+	allScriptsTimeout: 11000,
+	framework: 'jasmine2',
+	onPrepare: function () {
+	    browser.addMockModule('disableNgAnimate', disableNgAnimate);
+
+	    function disableNgAnimate() {
+	      angular.module('disableNgAnimate', []).run(['$animate', function($animate) {
+	        $animate.enabled(false);
+	      }]);
+	    };
+	},
+	jasmineNodeOpts: {
+		defaultTimeoutInterval: 60000,
+		showTiming: true
+	}
 }
