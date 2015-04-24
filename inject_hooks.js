a45fdc7471d7d7dbe4ce5252b661be9991841cb6
Mykhailo Miroshnikov <mykhailo.miroshnikov@gmail.com>
2015-04-24 14:50:28 +0300

    Improve inject_hooks.js

--- inject_hooks.js
+++ inject_hooks.js
@@ -1,10 +1,17 @@
 var fs = require('fs'),
-	hook;
+	exec = require('child_process').execSync,
+	hook,
+	commands,
+	HOOKS = '.git/hooks';
 
 hook = '#!/usr/bin/node\n' +
-'var exec = require(\'child_process\').execSync,\n' +
-	'output;\n' +
-'output = exec(\'protractor e2e/conf.js\');'
+'var exec = require(\'child_process\').execSync;\n';
 
-fs.renameSync('.git/hooks/pre-commit.sample', '.git/hooks/pre-commit')
+commands = fs.readFileSync('.pre-commit', 'utf-8');
+
+commands.split('\n').forEach(function (cmd) {
+	hook += 'exec(\'' + cmd + '\');';
+});
+
 fs.writeFileSync('.git/hooks/pre-commit', hook);
+exec('chmod +x .git/hooks/pre-commit');
